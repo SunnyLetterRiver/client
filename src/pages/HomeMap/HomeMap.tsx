2c26eb50e0a291a1b9e91218c009ec14ea5ae88b
wanwari <52253686+wanwari@users.noreply.github.com>
2024-10-22 20:41:48 -0400

    1 create location popup (#3)
    * install react-compare-slider
    
    * fix rendering mantine styles
    
    * create & style map popup

--- src/pages/HomeMap/HomeMap.tsx
+++ src/pages/HomeMap/HomeMap.tsx
@@ -1,14 +1,17 @@
-import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+import { MapContainer, TileLayer } from 'react-leaflet';
 import { LatLngTuple } from 'leaflet';
 import { ReactElement } from 'react';
 import { createLazyFileRoute } from '@tanstack/react-router';
+import { MapMarker } from './components/MapMarker';
 
 export const HomeMap = (): ReactElement => {
-  const position: LatLngTuple = [51.505, -0.09];
+  const latitude = 43.64253437503958;
+  const longitude = -79.3871372216558;
+  const center: LatLngTuple = [latitude, longitude];
 
   return (
     <MapContainer
-      center={position}
+      center={center}
       zoom={13}
       scrollWheelZoom={true}
       style={{ width: '100%', height: '100vh' }}
@@ -17,9 +20,7 @@ export const HomeMap = (): ReactElement => {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={position}>
-        <Popup></Popup>
-      </Marker>
+      <MapMarker latitude={latitude} longitude={longitude} />
     </MapContainer>
   );
 };
