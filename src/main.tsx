be485eac0093275f8c169e978bf9cefdc76c0f82
wanwari <contact@wanwari.ca>
2024-10-22 20:33:06 -0400

    fix rendering mantine styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,10 @@
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider, createRouter } from '@tanstack/react-router';
-
 import { routeTree } from './routeTree.gen';
+import { MantineProvider } from '@mantine/core';
+import '@mantine/core/styles.css';
+import '../src/assets/main.css';
 
 const router = createRouter({ routeTree });
 
@@ -17,7 +19,9 @@ if (!rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement);
   root.render(
     <StrictMode>
-      <RouterProvider router={router} />
+      <MantineProvider>
+        <RouterProvider router={router} />
+      </MantineProvider>
     </StrictMode>
   );
 }
